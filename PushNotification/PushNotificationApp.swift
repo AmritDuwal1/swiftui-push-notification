62129b315dfc7b3d40bd20981ae993de1e2b6681
AmritDuwal1 <61304552+AmritDuwal1@users.noreply.github.com>
2024-04-25 14:55:17 +0545

    Initial Commit

---
+++ PushNotification/PushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationApp.swift
+//  PushNotification
+//
+//  Created by Amrit Duwal on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
